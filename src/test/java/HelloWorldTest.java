5c6f5ff5125a95f9adf4c6e81722639168a2d712
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-02-05 17:12:20 +0300

    Занятие 1, задание 4.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -20,4 +20,15 @@ public class HelloWorldTest {
         System.out.println("Hello from Ruslan");
     }
 
+    // Тест, который отправляет GET-запрос по адресу: https://playground.learnqa.ru/api/get_text
+    // Выведено содержимое текста в ответе на запрос
+    @Test
+    public void testSendGETRequestToAddress(){
+
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_text")
+                .andReturn();
+        response.prettyPrint();
+    }
+
 }
