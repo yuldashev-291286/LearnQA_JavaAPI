c33e9c32f3219f070322832d556ff7cb130292d7
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-02-05 16:06:58 +0300

    Первый тест API

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,16 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import org.junit.jupiter.api.Test;
 
 public class HelloWorldTest {
 
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello world");
+
+        Response response = RestAssured
+                .get("http://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
     }
 }
