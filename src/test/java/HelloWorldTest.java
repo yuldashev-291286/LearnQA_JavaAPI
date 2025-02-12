7510a9c1b690a8fee3f58125ec814c390562815c
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-02-12 19:27:47 +0300

    Занятие 2, учебные тесты.

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -174,5 +174,20 @@ public class HelloWorldTest {
 
     }
 
+    // Занятие №2. ДЗ №1. Ex5: Парсинг JSON.
+    @Test
+    public void testRestAssured7(){
+
+        JsonPath response = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+
+        response.prettyPrint();
+
+        String TextOfTheSecondMessage = response.getString("messages[1].message");
+        System.out.println(TextOfTheSecondMessage);
+
+    }
+
 
 }
