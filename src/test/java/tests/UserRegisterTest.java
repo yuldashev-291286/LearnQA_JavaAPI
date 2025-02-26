d611a3f7bc42819316f6091f559df3817533c0ea
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-02-26 12:26:44 +0300

    Занятие 4. Ex15: Тесты на метод user. Учебный тест №1.

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends BaseTestCase {
+
+    // Занятие 4. Ex15: Тесты на метод user. Учебный тест №1.
+    @Test
+    public void testCreateUserWithExistingEmail(){
+        String email = "vinkotov@example.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("password", "123");
+        userData.put("username", "learnqa");
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        //System.out.println(responseCreateAuth.asString());
+        //System.out.println(responseCreateAuth.statusCode());
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+
+    }
+
+
+
+}
