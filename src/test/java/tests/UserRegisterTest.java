433df33daa07e4d55f0bef024957339bd17f2cba
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-02-26 13:32:41 +0300

    Занятие 4. Ex15: Тесты на метод user. ДЗ 1.

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -5,6 +5,8 @@ import io.restassured.response.Response;
 import lib.Assertions;
 import lib.BaseTestCase;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -29,14 +31,134 @@ public class UserRegisterTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/")
                 .andReturn();
 
-        //System.out.println(responseCreateAuth.asString());
-        //System.out.println(responseCreateAuth.statusCode());
-
         Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
         Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
 
     }
 
+    // Занятие 4. Ex15: Тесты на метод user. ДЗ 1. Тест №1: Создание пользователя с некорректным email - без символа @.
+    @Test
+    public void testCreateUserWithInvalidEmailWithoutTheATSymbol(){
+        String email = "vinkotov_example.com";
 
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("password", "123");
+        userData.put("username", "learnqa");
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Invalid email format");
+
+    }
+
+    // Занятие 4. Ex15: Тесты на метод user. ДЗ 1. Тест №2: Создание пользователя без указания одного из полей. С помощью @ParameterizedTest необходимо проверить,
+    // что отсутствие любого параметра не дает зарегистрировать пользователя.
+    // Параметры: vinkotov@example.com, 1234, username, firstName, lastName
+    @ParameterizedTest
+    @CsvSource({
+            "'', 1234, username, firstName, lastName",
+            "vinkotov@example.com, '', username, firstName, lastName",
+            "vinkotov@example.com, 1234, '', firstName, lastName",
+            "vinkotov@example.com, 1234, username, '', lastName",
+            "vinkotov@example.com, 1234, username, firstName, ''",
+    })
+    public void testCreateUserWithoutSpecifyingOneOfFields(String Email, String Password, String Username, String FirstName, String LastName){
+
+        String email;
+        String password;
+        String username;
+        String firstName;
+        String lastName;
+
+        email = Email;
+        password = Password;
+        username = Username;
+        firstName = FirstName;
+        lastName = LastName;
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData.put("password", password);
+        userData.put("username", username);
+        userData.put("firstName", firstName);
+        userData.put("lastName", lastName);
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        if (email == null | email == ""){
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: email");
+        }else if (password == null | password == ""){
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: password");
+        }else if (username == null | username == ""){
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: username");
+        }else if (firstName == null | firstName == ""){
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: firstName");
+        }else if (lastName == null | lastName == ""){
+            Assertions.assertResponseTextEquals(responseCreateAuth, "The following required params are missed: lastName");
+        }
+
+
+    }
+
+    // Занятие 4. Ex15: Тесты на метод user. ДЗ 1. Тест №3: Создание пользователя с очень коротким именем в один символ.
+    @Test
+    public void testCreateUserWithVeryShortOneCharacterName(){
+        String shortUsername = "W";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", "vinkotov@example.com");
+        userData.put("password", "123");
+        userData.put("username", shortUsername);
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too short");
+
+    }
+
+    // Занятие 4. Ex15: Тесты на метод user. ДЗ 1. Тест №4: Создание пользователя с очень длинным именем - длиннее 250 символов.
+    @Test
+    public void testCreateUserWithVeryLongNameLongerThan250Characters(){
+        String longUsername = "12345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890" +
+                "12345678901234567890123456789012345678901234567890";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", "vinkotov@example.com");
+        userData.put("password", "123");
+        userData.put("username", longUsername);
+        userData.put("firstName", "learnqa");
+        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        Assertions.assertResponseTextEquals(responseCreateAuth, "The value of 'username' field is too long");
+
+    }
 
 }
