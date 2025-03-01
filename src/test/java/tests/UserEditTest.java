9196970b6cd03eb59aa8edcad588df15df07698c
yuldashev-291286 <56519643+yuldashev-291286@users.noreply.github.com>
2025-03-01 14:36:03 +0300

    Занятие 4. Учебный тест №7.

--- src/test/java/tests/UserEditTest.java
+++ src/test/java/tests/UserEditTest.java
@@ -11,6 +11,9 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class UserEditTest extends BaseTestCase {
 
     // Занятие 4. Учебный тест №7.
@@ -25,7 +28,9 @@ public class UserEditTest extends BaseTestCase {
                 .post("https://playground.learnqa.ru/api/user/")
                 .jsonPath();
 
-        String userId = "7548";//responseCreateAuth.getString("id");
+        // Url: https://playground.learnqa.ru/api/user/, всегда возвращает: {"error":"Wrong HTTP method"}
+
+        String userId = "1"; //responseCreateAuth.getString("id");
 
         // LOGIN
         Map<String, String> authData = new HashMap<>();
@@ -43,10 +48,19 @@ public class UserEditTest extends BaseTestCase {
         Map<String, String> editData = new HashMap<>();
         editData.put("firstName", newName);
 
+        // Url: https://playground.learnqa.ru/api/user/" + userId.
+        // userId=0, возвращает: {"error":"Wrong HTTP method"}
+        // userId=1, возвращает: {"username":"Lana"}
+        // userId=2, возвращает: {"username":"Vitaliy"}
+        // userId=3, возвращает: {"username":"arsbatyrov"}
+        // userId>3, возвращает: User not found
+
         Response responseEditUser = RestAssured
                 .given()
-                .header("x-csrf-token", /*this.getHeader(responseGetAuth, "x-csrf-token")*/ "x-csrf-token")
-                .cookie("auth_sid", /*this.getCookie(responseGetAuth, "auth_sid")*/ "auth_sid")
+                //.header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .header("x-csrf-token", "x-csrf-token")
+                //.cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .cookie("auth_sid", "auth_sid")
                 .body(editData)
                 .put("https://playground.learnqa.ru/api/user/" + userId)
                 .andReturn();
@@ -54,13 +68,41 @@ public class UserEditTest extends BaseTestCase {
         // GET
         Response responseUserData = RestAssured
                 .given()
-                .header("x-csrf-token", /*this.getHeader(responseGetAuth, "x-csrf-token")*/ "x-csrf-token")
-                .cookie("auth_sid", /*this.getCookie(responseGetAuth, "auth_sid")*/ "auth_sid")
+                //.header("x-csrf-token", this.getHeader(responseGetAuth, "x-csrf-token"))
+                .header("x-csrf-token", "x-csrf-token")
+                //.cookie("auth_sid", this.getCookie(responseGetAuth, "auth_sid"))
+                .cookie("auth_sid", "auth_sid")
                 .get("https://playground.learnqa.ru/api/user/" + userId)
                 .andReturn();
 
-        System.out.println(responseUserData.asString());
         //Assertions.asserJsonByName(responseUserData, "firstName", newName);
+
+        Integer number = Integer.valueOf(userId);
+        if (number == 0){
+            System.out.print("Неправильный метод HTTP: ");
+            System.out.println(responseUserData.asString());
+
+        }
+        else if (number == 1){
+            Assertions.asserJsonByName(responseUserData, "username", "Lana");
+            System.out.println(responseUserData.asString());
+
+        }else if (number == 2){
+            Assertions.asserJsonByName(responseUserData, "username", "Vitaliy");
+            System.out.println(responseUserData.asString());
+
+        }
+        else if (number == 3){
+            Assertions.asserJsonByName(responseUserData, "username", "arsbatyrov");
+            System.out.println(responseUserData.asString());
+
+        }else if (number > 3){
+            assertTrue(responseUserData.asString().equals("User not found"), "User not found");
+            System.out.println(responseUserData.asString());
+
+        }
+
+
 
     }
 
